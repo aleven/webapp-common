8f8b002ae2d0bc76e2beea56e556a62aeb2b2f1b
Mirco <amirco@gmail.com>
2012-08-28 15:13:56 +0200

    start write a generic entity filter

--- src/it/espressoft/jpa2/JPAEntityFilter.java
+++ src/it/espressoft/jpa2/JPAEntityFilter.java
@@ -7,8 +7,11 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import org.apache.commons.lang3.StringUtils;
+
 public abstract class JPAEntityFilter<T extends Serializable> {
 
+	private static final String JOLLY_CHAR = "%";
 	EntityManagerFactory em;
 	CriteriaBuilder criteriaBuilder;
 	CriteriaQuery<T> criteriaQuery;
@@ -16,7 +19,7 @@ public abstract class JPAEntityFilter<T extends Serializable> {
 
 	public CriteriaQuery<T> getCriteria(Class<T> clazz, EntityManagerFactory em) {
 
-		// CriteriaBuilder 
+		// CriteriaBuilder
 		criteriaBuilder = em.getCriteriaBuilder();
 
 		criteriaQuery = criteriaBuilder.createQuery(clazz);
@@ -34,6 +37,34 @@ public abstract class JPAEntityFilter<T extends Serializable> {
 	}
 
 	public abstract void buildWhere(CriteriaQuery<T> criteriaQuery, CriteriaBuilder criteriaBuilder, Root<T> root);
-	
+
 	public abstract void buildSort(CriteriaQuery<T> criteriaQuery, CriteriaBuilder criteriaBuilder, Root<T> root);
+
+	private String semeRicerca;
+
+	public String getSemeRicerca() {
+		return semeRicerca;
+	}
+
+	public void setSemeRicerca(String semeRicerca) {
+		this.semeRicerca = semeRicerca;
+	}
+
+	/**
+	 * 
+	 * @param semeRicerca
+	 */
+	public String getSemeRicercaForLike() {
+		String res = semeRicerca;
+
+		if (StringUtils.isNotEmpty(res)) {
+			if (res.startsWith(JOLLY_CHAR) || res.endsWith(JOLLY_CHAR)) {
+				// OK
+			} else {
+				res = JOLLY_CHAR + semeRicerca + JOLLY_CHAR;
+			}
+		}
+
+		return res;
+	}
 }
