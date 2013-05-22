b7bcbd52d56501211aa42b0b9b99321d6760f5c0
Mirco <amirco@gmail.com>
2013-05-22 12:11:46 +0200

    funzioni read e write Lines per simulare righe files su testo

--- src/it/attocchi/utils/StringFunc.java
+++ src/it/attocchi/utils/StringFunc.java
@@ -1,8 +1,13 @@
 package it.attocchi.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class StringFunc {
+
+	public static final String NEW_LINE = "\r\n";
 
 	public static String concat(String separator, String... strings) {
 		StringBuilder sb = new StringBuilder();
@@ -24,5 +29,23 @@ public class StringFunc {
 
 	public static boolean contains(String string, String subString) {
 		return string != null && !string.isEmpty() && string.indexOf(subString) >= 0;
+	}
+
+	public static List<String> readLines(String aText) {
+		List<String> res = null;
+
+		if (StringUtils.isNotBlank(aText))
+			res = Arrays.asList(StringUtils.split(aText, NEW_LINE));
+
+		return res;
+	}
+
+	public static String writeLines(List<String> lines, String defaultValueIfEmpty) {
+		String res = defaultValueIfEmpty;
+
+		if (ListUtils.isNotEmpty(lines))
+			res = StringUtils.join(lines.toArray(), NEW_LINE);
+
+		return res;
 	}
 }
