dae6532a70880075b783916bb8e1333b2d3819cc
Mirco <amirco@gmail.com>
2013-11-21 11:13:25 +0100

    filtro per configurare il set di caratteri delle request, trovato come rimedio al problema del primo post jsf che sembra non itercettare il charset

---
+++ src/it/attocchi/web/filters/CharacterEncodingFilter.java
@@ -0,0 +1,31 @@
+package it.attocchi.web.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+// @WebFilter("/*")
+public class CharacterEncodingFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+}
