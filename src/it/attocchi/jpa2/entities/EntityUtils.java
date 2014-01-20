2fd94043a5eb98aae515bb3e58c5a14f5c3b94c7
Mirco <amirco@gmail.com>
2014-01-20 11:53:12 +0100

    ricava lista di id da lista entita

---
+++ src/it/attocchi/jpa2/entities/EntityUtils.java
@@ -0,0 +1,22 @@
+package it.attocchi.jpa2.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityUtils {
+	
+	/**
+	 * 
+	 * @param list
+	 * @return
+	 */
+	public static <T extends AbstractEntityMarksWithIdLong> List<Long> toListOfId(List<T> list) {
+		List<Long> ids = new ArrayList<Long>();
+		for (T entity : list) {
+			ids.add(entity.getId());
+		}
+		return ids;
+	}
+	
+}
+
