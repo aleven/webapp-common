0c55a31920eac1b12331da77e2734fca5f62cfa7
Mirco <amirco@gmail.com>
2014-06-05 13:09:10 +0200

    spostato test dateutils

--- src/test/java/it/attocchi/test/TestDateUtils.java
+++ src/test/java/it/attocchi/test/TestDateUtils.java
@@ -1,25 +1,7 @@
-/*
-    Copyright (c) 2012,2013 Mirco Attocchi
-	
-    This file is part of WebAppCommon.
-
-    WebAppCommon is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    WebAppCommon is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with WebAppCommon.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package it.attocchi.test;
 
-import it.attocchi.utils.DateUtils;
+import it.attocchi.utils.DateUtilsLT;
+import it.webappcommon.lib.DateUtils;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -35,14 +17,10 @@ public class TestDateUtils {
 	static Calendar cal1,
 		cal2;
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
+		testDifferenza();
 
-		 testDifferenza();
-
-//		testSuddivisione();
+		// testSuddivisione();
 	}
 
 	private static void testDifferenza() {
@@ -50,8 +28,8 @@ public class TestDateUtils {
 		cal1 = Calendar.getInstance();
 		cal2 = Calendar.getInstance();
 
-		cal1.set(2013, 11 - 1, 11, 16, 00);
-		cal2.set(2013, 11 - 1, 19, 18, 00);
+		cal1.set(2014, 06 - 1, 03, 16, 00);
+		cal2.set(2014, 07 - 1, 03, 16, 00);
 
 		inizio = cal1.getTime();
 		fine = cal2.getTime();
@@ -66,16 +44,25 @@ public class TestDateUtils {
 		System.out.println(DateUtils.getDaysBetween(cal2, cal1));
 		System.out.println(DateUtils.getDaysBetween(cal1, cal2));
 
+		System.out.println(DateUtils.differenzaInSettimane(inizio, fine));
+		System.out.println(DateUtils.differenzaInMesi(inizio, fine));
 	}
 
 	private static void testSuddivisione() {
 
 		DateTime now = new DateTime();
-		List<Interval> list = DateUtils.splitDuration(now, now.plusDays(10), 3, 60 * 60 * 24 * 7 * 1000);
+		List<Interval> list = it.attocchi.utils.DateUtils.splitDuration(now, now.plusDays(10), 3, 60 * 60 * 24 * 7 * 1000);
 
 		for (Interval i : list) {
 			System.out.println(i.getStart() + " - " + i.getEnd() + " - " + i.toDurationMillis());
 		}
 
+	}
+
+	private static void isBetween() {
+		System.out.println(DateUtilsLT.isBetween(new Date(), new Date(), new Date()));
+		System.out.println(DateUtilsLT.isBetween(new Date(), DateUtilsLT.addDays(new Date(), -1), new Date()));
+		System.out.println(DateUtilsLT.isBetween(new Date(), DateUtilsLT.addDays(new Date(), -10), DateUtilsLT.addDays(new Date(), 10)));
+		System.out.println(DateUtilsLT.isBetween(new Date(), DateUtilsLT.addDays(new Date(), 10), DateUtilsLT.addDays(new Date(), 50)));
 	}
 }
