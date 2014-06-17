192d6b5ef2ea5ebcdb4338434df5aa71167962ff
Mirco <amirco@gmail.com>
2014-06-17 17:41:43 +0200

    rimosso commenti todo

--- src/main/java/it/attocchi/mail/utils/ssl/DummyTrustManager.java
+++ src/main/java/it/attocchi/mail/utils/ssl/DummyTrustManager.java
@@ -9,19 +9,14 @@ public class DummyTrustManager implements X509TrustManager {
 
 	@Override
 	public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public X509Certificate[] getAcceptedIssuers() {
-		// TODO Auto-generated method stub
 		// return null;
 		return new X509Certificate[0];
 	}
