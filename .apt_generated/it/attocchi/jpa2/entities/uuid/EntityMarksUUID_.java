9111483e3843d4dbe697bb5339694580acd7f75e
Mirco <amirco@gmail.com>
2014-06-23 15:17:49 +0200

    aggiunto classi generate da jpamodelgen

---
+++ .apt_generated/it/attocchi/jpa2/entities/uuid/EntityMarksUUID_.java
@@ -0,0 +1,20 @@
+package it.attocchi.jpa2.entities.uuid;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(EntityMarksUUID.class)
+public abstract class EntityMarksUUID_ {
+
+	public static volatile SingularAttribute<EntityMarksUUID, Date> dataModifica;
+	public static volatile SingularAttribute<EntityMarksUUID, String> uuidUtenteModifica;
+	public static volatile SingularAttribute<EntityMarksUUID, Date> dataCancellazione;
+	public static volatile SingularAttribute<EntityMarksUUID, String> uuidUtenteCancellazione;
+	public static volatile SingularAttribute<EntityMarksUUID, Date> dataCreazione;
+	public static volatile SingularAttribute<EntityMarksUUID, String> uuidUtenteCreazione;
+
+}
+
