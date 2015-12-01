71e45cbfd6bec7e10d3a7c9668ddda7673b13a9e
Mirco <amirco@gmail.com>
2015-12-01 16:38:16 +0100

    rimosso metodo di test

--- src/main/java/it/attocchi/mail/utils/MailSender.java
+++ src/main/java/it/attocchi/mail/utils/MailSender.java
@@ -336,28 +336,28 @@ public class MailSender {
 		}
 	}
 
-	@Deprecated
-	public void sendHtmlEmail() throws Exception {
-		// Create the email message
-		HtmlEmail email = new HtmlEmail();
-		email.setHostName("mail.myserver.com");
-		email.addTo("jdoe@somewhere.org", "John Doe");
-		email.setFrom("me@apache.org", "Me");
-		email.setSubject("Test email with inline image");
-
-		// embed the image and get the content id
-		URL url = new URL("http://www.apache.org/images/asf_logo_wide.gif");
-		String cid = email.embed(url, "Apache logo");
-
-		// set the html message
-		email.setHtmlMsg("<html>The apache logo - <img src=\"cid:" + cid + "\"></html>");
-
-		// set the alternative message
-		email.setTextMsg("Your email client does not support HTML messages");
-
-		// send the email
-		email.send();
-	}
+//	@Deprecated
+//	private void sendHtmlEmail() throws Exception {
+//		// Create the email message
+//		HtmlEmail email = new HtmlEmail();
+//		email.setHostName("mail.myserver.com");
+//		email.addTo("jdoe@somewhere.org", "John Doe");
+//		email.setFrom("me@apache.org", "Me");
+//		email.setSubject("Test email with inline image");
+//
+//		// embed the image and get the content id
+//		URL url = new URL("http://www.apache.org/images/asf_logo_wide.gif");
+//		String cid = email.embed(url, "Apache logo");
+//
+//		// set the html message
+//		email.setHtmlMsg("<html>The apache logo - <img src=\"cid:" + cid + "\"></html>");
+//
+//		// set the alternative message
+//		email.setTextMsg("Your email client does not support HTML messages");
+//
+//		// send the email
+//		email.send();
+//	}
 
 	/**
 	 * Extract multi recipient from a string separed with ; or ,
