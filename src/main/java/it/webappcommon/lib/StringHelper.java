e66ae69ac97414e54abcc386a0f94010237d5598
Mirco <amirco@gmail.com>
2017-09-05 15:29:50 +0200

    aggiunto metodo like
    per effettuare like di string come in sql

--- src/main/java/it/webappcommon/lib/StringHelper.java
+++ src/main/java/it/webappcommon/lib/StringHelper.java
@@ -300,5 +300,24 @@ public class StringHelper {
 			res = StringUtils.join(lines.toArray(), NEW_LINE);
 
 		return res;
+	}
+	
+	/**
+	 * 
+	 * @param str
+	 * @param expr
+	 * @return
+	 */
+	public static boolean like(String str, String expr) {
+		if (str == null || expr == null) 
+			return false;
+		
+	    expr = expr.toLowerCase(); // ignoring locale for now
+	    expr = expr.replace(".", "\\."); // "\\" is escaped to "\" (thanks, Alan M)
+	    // ... escape any other potentially problematic characters here
+	    expr = expr.replace("?", ".");
+	    expr = expr.replace("%", ".*");
+	    str = str.toLowerCase();
+	    return str.matches(expr);
 	}	
 }
