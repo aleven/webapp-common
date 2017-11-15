5ea5ca2ebb8e4661e73bb057798b2cecb53aea40
Mirco <amirco@gmail.com>
2017-11-15 10:24:53 +0100

    aggiunti test data esiste

--- src/test/java/it/attocchi/test/TestDateUtilsLT.java
+++ src/test/java/it/attocchi/test/TestDateUtilsLT.java
@@ -12,16 +12,46 @@ public class TestDateUtilsLT {
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Test
-	public void testExists() {
+	public void testExistsFalse() {
 		logger.info("testExists");
 		int anno = 2017;
 		int mese = 1;
 		int giorno = 30;
-		boolean res = DateUtilsLT.exists(anno, mese, giorno);
-		
+		boolean res = DateUtilsLT.exists(anno, mese, giorno);		
 		logger.info("{} {} {} = {}", anno, mese, giorno, res);
-		
 		Assert.assertFalse(res);
 	}
 
+	@Test
+	public void testExistsTrue() {
+		logger.info("testExists");
+		int anno = 2017;
+		int mese = 1;
+		int giorno = 28;
+		boolean res = DateUtilsLT.exists(anno, mese, giorno);		
+		logger.info("{} {} {} = {}", anno, mese, giorno, res);
+		Assert.assertTrue(res);
+	}
+	
+	@Test
+	public void testExistsFalseBisestile() {
+		logger.info("testExists");
+		int anno = 2017;
+		int mese = 1;
+		int giorno = 29;
+		boolean res = DateUtilsLT.exists(anno, mese, giorno);		
+		logger.info("{} {} {} = {}", anno, mese, giorno, res);
+		Assert.assertFalse(res);
+	}
+	
+	@Test
+	public void testExistsTrueBisestile() {
+		logger.info("testExists");
+		int anno = 2020;
+		int mese = 1;
+		int giorno = 29;
+		boolean res = DateUtilsLT.exists(anno, mese, giorno);		
+		logger.info("{} {} {} = {}", anno, mese, giorno, res);
+		Assert.assertTrue(res);
+	}	
 }
