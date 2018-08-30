ec5f83ff8d49f1cebc85c8d79042d577289cfc53
Mirco <amirco@gmail.com>
2018-08-30 16:49:12 +0200

    aggiunto supporto a validazione entity basata su profilo utente

--- src/main/java/it/attocchi/jpa2/entities/IEntityWithValidation.java
+++ src/main/java/it/attocchi/jpa2/entities/IEntityWithValidation.java
@@ -2,6 +2,16 @@ package it.attocchi.jpa2.entities;
 
 import it.attocchi.jsf2.exceptions.ValidationException;
 
+/**
+ * Interfaccia identificazione Entity che richiedono Validazione
+ */
 public interface IEntityWithValidation {
-	public boolean validate() throws ValidationException;
+
+	/**
+	 * metodo che effettua validazione dell'entity
+	 * @param utentePrivilegiato identifica se utente correntemente loggato è identificato come ADMIN e/o GRUPPO GESTORE MODULO
+	 * @return true se oggetto è validato
+	 * @throws ValidationException eccezzione che identifica errore di validazione
+	 */
+	public boolean validate(boolean utentePrivilegiato) throws ValidationException;
 }
